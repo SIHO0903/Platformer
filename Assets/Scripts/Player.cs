8f74024c5e51f12af8a553a0a94f6351e071d071
Siho Choi <tlgh10@naver.com>
2023-05-11 14:53:16 +0900

    Player Move

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("# Move")]
+    [SerializeField] float speed;
+    [SerializeField] float jumpPower;
+
+    Rigidbody2D rigid;
+    SpriteRenderer sprite;
+    Animator anim;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    private void Move()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        rigid.velocity = new Vector2(moveX * speed, rigid.velocity.y);
+
+        if (moveX != 0)
+            sprite.flipX = moveX < 0 ? true : false;
+
+        anim.SetBool("Run", moveX != 0);
+    }
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid.velocity = Vector2.up * jumpPower;
+
+        }
+        anim.SetInteger("Jump", Mathf.RoundToInt(rigid.velocity.y));
+    }
+
+}
