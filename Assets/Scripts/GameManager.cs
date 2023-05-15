9fd3823454608da9c67ed32b62719b513a28ffa0
Siho Choi <tlgh10@naver.com>
2023-05-15 22:35:36 +0900

    HUD alpha

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,9 +6,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
     public Player player;
+
+    public int coin;
+    public int stage;
+
+    public float gameTime;
     void Awake()
     {
         Instance= this;
+    }
+    private void Update()
+    {
+        gameTime += Time.deltaTime;
     }
 
 }
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HUD : MonoBehaviour
+{
+    TextMeshProUGUI text;
+    enum HudType { Coin, Stage, Time}
+    [SerializeField] HudType type;
+    private void Awake()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void LateUpdate()
+    {
+        float gameTime = GameManager.Instance.gameTime;
+        switch (type)
+        {
+            case HudType.Coin:
+                text.text = string.Format($"X {GameManager.Instance.coin}");
+                break;
+            case HudType.Stage:
+                text.text = string.Format($"STAGE {GameManager.Instance.stage}");
+                break; 
+            case HudType.Time:
+                text.text = string.Format($"TIME {Mathf.FloorToInt(gameTime / 60)}:{Mathf.FloorToInt(gameTime % 60)}");
+                break;
+        }
+    }
+}
