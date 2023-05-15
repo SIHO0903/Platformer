5f4d5303b6f0f4761560f8f6ae9a4230cd4a32af
Siho Choi <tlgh10@naver.com>
2023-05-15 14:32:24 +0900

    BackgroundScrolling1.0

--- Assets/Scripts/BackgroundScrolling.cs
+++ Assets/Scripts/BackgroundScrolling.cs
@@ -2,16 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BackgroundScrolling : MonoBehaviour
+public class BackGroundScrolling : MonoBehaviour
 {
-    [SerializeField] GameObject[] backgrounds;
-    void Start()
+    [SerializeField]
+    [Range(-1.0f, 1.0f)]
+    private float moveSpeed = 0.1f;
+    private Material material;
+
+    private float distance;
+    private Vector3 cameraStartPosition;
+    void Awake()
     {
-        
+        material = GetComponent<Renderer>().material;
+
+        cameraStartPosition = transform.parent.transform.position;
+
     }
 
     void Update()
     {
-        
+        distance = transform.parent.transform.position.x - cameraStartPosition.x;
+        material.SetTextureOffset("_MainTex", new Vector2(distance,0) * moveSpeed);
     }
 }
